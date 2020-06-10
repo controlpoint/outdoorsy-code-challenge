f8dfa0b4518e0f11a5d0ca2c58428966f96ebda6
Alonzo Wilkins <mal.inLatin@gmail.com>
2020-06-10 10:50:28 -0700

    Initial Commit

---
+++ Outdoorsy Code ChallengeTests/Outdoorsy_Code_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Outdoorsy_Code_ChallengeTests.swift
+//  Outdoorsy Code ChallengeTests
+//
+//  Created by Alonzo Wilkins on 6/10/20.
+//  Copyright © 2020 Anchor Point. All rights reserved.
+//
+
+import XCTest
+@testable import Outdoorsy_Code_Challenge
+
+class Outdoorsy_Code_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
